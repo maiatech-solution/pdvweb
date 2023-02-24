bf06b0c6bf8832f7b1144b8d1c21cb2ae910385c
maiatech-solution <drikomaia89@gmail.com>
2023-02-24 15:47:48 -0300

    finalizando tela de estoque

--- src/assets/pages/Estoque.tsx
+++ src/assets/pages/Estoque.tsx
@@ -1,19 +1,16 @@
-import { BsFillPencilFill, BsXCircleFill } from "react-icons/bs"
-import { Produtos } from "../../date/produtos"
-import { Footer } from "../components/Footer/footer"
-import { Header } from "../components/Header/header"
-import Modal from "../components/ModalProd/modal"
-import { TableList } from "../components/TableList/tableList"
-import LogoEmpresa from '../imgs/maiatech.png'
+import { BsFillPencilFill, BsXCircleFill } from "react-icons/bs";
+import { Produtos } from "../../date/produtos";
+import { Footer } from "../components/Footer/footer";
+import { Header } from "../components/Header/header";
+import Modal from "../components/ModalProd/modal";
+import { TableList } from "../components/TableList/tableList";
+import LogoEmpresa from "../imgs/maiatech.png";
 
 export const Estoque = () => {
-
-
   return (
-    <div >
-      <Header text="Estoque" className='text-4xl text-cyan-700 mb-4'/>
+    <div>
+      <Header text="Estoque" className="text-4xl text-cyan-700 mb-4" />
       <main className="h-[85vh] w-[70%] m-auto">
-
         <div className="h-[30%] border-2 rounded-md bg-cyan-700 flex mb-4">
           <div className="w-[30%] h-[100%] flex justify-center bg-transparent items-center">
             <div className="w-[200px] border">
@@ -21,32 +18,80 @@ export const Estoque = () => {
             </div>
           </div>
           <div className="w-[70%] h-[100%] flex flex-col justify-center gap-4 bg-transparent items-center">
-            <div className=" w-[90%] flex gap-4 bg-transparent" >
+            <div className=" w-[90%] flex gap-4 bg-transparent">
               <div className="w-[25%] flex flex-col">
-                <label htmlFor="codProd" className="bg-gray-600 w-[100%] border pl-2 text-gray-100 ">Cód. Produto: </label>
-                <input type="number" name="codProd" id="codProd" className="bg-white outline-none pl-2" />
+                <label
+                  htmlFor="codProd"
+                  className="bg-gray-600 w-[100%] border pl-2 text-gray-100 "
+                >
+                  Cód. Produto:{" "}
+                </label>
+                <input
+                  type="number"
+                  name="codProd"
+                  id="codProd"
+                  className="bg-white outline-none pl-2"
+                />
               </div>
               <div className="w-[75%] flex flex-col ">
-                <label htmlFor="descProd" className="bg-gray-600 w-[100%] border pl-2 text-gray-100 ">Descrição do produto: </label>
-                <input type="text" name="descProd" id="descProd" className=" bg-white outline-none pl-2" />
+                <label
+                  htmlFor="descProd"
+                  className="bg-gray-600 w-[100%] border pl-2 text-gray-100 "
+                >
+                  Descrição do produto:{" "}
+                </label>
+                <input
+                  type="text"
+                  name="descProd"
+                  id="descProd"
+                  className=" bg-white outline-none pl-2"
+                />
               </div>
             </div>
             <div className="w-[90%] gap-4 flex justify-between bg-transparent">
               <div className=" w-[25%]  flex flex-col bg-transparent">
-                <label htmlFor="catProd" className="bg-gray-600 w-[100%] border pl-2 text-gray-100 ">Categoria: </label>
-                <select name="catProd" id="catProd" className=" outline-none bg-white p-2">
+                <label
+                  htmlFor="catProd"
+                  className="bg-gray-600 w-[100%] border pl-2 text-gray-100 "
+                >
+                  Categoria:{" "}
+                </label>
+                <select
+                  name="catProd"
+                  id="catProd"
+                  className=" outline-none bg-white p-2"
+                >
                   <option value="">Selecione...</option>
                   <option value="informatica">Informática</option>
                   <option value="moinitores">Monitores...</option>
                 </select>
               </div>
               <div className="w-[25%] flex flex-col bg-transparent ">
-                <label htmlFor="saldoProd" className="bg-gray-600 border w-[100%] pl-2 text-gray-100">Saldo: </label>
-                <input type="number" name="saldoProd" id="saldoProd " className="bg-white outline-none p-2" />
+                <label
+                  htmlFor="saldoProd"
+                  className="bg-gray-600 border w-[100%] pl-2 text-gray-100"
+                >
+                  Saldo:{" "}
+                </label>
+                <input
+                  type="number"
+                  name="saldoProd"
+                  id="saldoProd "
+                  className="bg-white outline-none p-2"
+                />
               </div>
               <div className="w-[25%] flex flex-col bg-transparent ">
-                <label htmlFor="unidProd" className="bg-gray-600 border w-[100%] pl-2 text-gray-100 ">Unidade: </label>
-                <select name="unidProd" id="unidProd" className="outline-none bg-white p-2">
+                <label
+                  htmlFor="unidProd"
+                  className="bg-gray-600 border w-[100%] pl-2 text-gray-100 "
+                >
+                  Unidade:{" "}
+                </label>
+                <select
+                  name="unidProd"
+                  id="unidProd"
+                  className="outline-none bg-white p-2"
+                >
                   <option value="">Selecione...</option>
                   <option value="unidade">UN</option>
                   <option value="metro">M</option>
@@ -56,37 +101,61 @@ export const Estoque = () => {
                 </select>
               </div>
               <div className="w-[25%] flex flex-col bg-transparent ">
-                <label htmlFor="precoProd" className="bg-gray-600 border w-[100%] pl-2 text-gray-100">Preço: </label>
-                <input type="number" name="precoProd" id="precoProd" className="bg-white outline-none p-2" />
+                <label
+                  htmlFor="precoProd"
+                  className="bg-gray-600 border w-[100%] pl-2 text-gray-100"
+                >
+                  Preço:{" "}
+                </label>
+                <input
+                  type="number"
+                  name="precoProd"
+                  id="precoProd"
+                  className="bg-white outline-none p-2"
+                />
               </div>
             </div>
-            <button className="text-white bg-gray-600 border hover:bg-gray-700 w-[90%] p-2">Salvar</button>
+            <button className="text-white bg-gray-600 border hover:bg-gray-700 w-[90%] p-2">
+              Salvar
+            </button>
           </div>
-
         </div>
 
         <div className="w-[100%] p-2 h-[10%] gap-2 m-auto mb-4 border border-gray-400 flex items-center justify-center bg-transparent">
           <div className="w-[50%]">
-            <input type="text" placeholder="Pesquisar produto" className="bg-white w-[100%] outline-none p-2 border border-gray-400" />
+            <input
+              type="text"
+              placeholder="Pesquisar produto"
+              className="bg-white w-[100%] outline-none p-2 border border-gray-400"
+            />
           </div>
 
           <div className=" p-2 w-[30%] flex flex-col bg-transparent ">
-            <label htmlFor="catProd" className="bg-gray-600 w-[100%]  pl-2 text-gray-100 ">Categoria: </label>
-            <select name="catProd" id="catProd" className=" outline-none bg-white">
+            <label
+              htmlFor="catProd"
+              className="bg-gray-600 w-[100%]  pl-2 text-gray-100 "
+            >
+              Categoria:{" "}
+            </label>
+            <select
+              name="catProd"
+              id="catProd"
+              className=" outline-none bg-white"
+            >
               <option value="">Selecione...</option>
               <option value="informatica">Informática</option>
               <option value="moinitores">Monitores...</option>
             </select>
           </div>
-          
+
           <div className="w-[20%] border flex items-center justify-center">
-            <button className="text-white w-[100%] bg-gray-600 border hover:bg-gray-700 p-2">Filtrar</button>
+            <button className="text-white w-[100%] bg-gray-600 border hover:bg-gray-700 p-2">
+              Filtrar
+            </button>
           </div>
         </div>
 
-
         <div className="h-[45%] w-[100%] mb-8 overflow-auto bg-cyan-200">
-
           <table className="table-auto w-full rounded-md ">
             <thead className="theader-estoque">
               <tr className="tr-theader">
@@ -99,28 +168,32 @@ export const Estoque = () => {
               </tr>
             </thead>
 
-            {Produtos.map(item => (
-
-              <tbody className="tbody-estoque" >
+            {Produtos.map((item) => (
+              <tbody className="tbody-estoque">
                 <tr key={item.cod}>
-                  <td >{item.cod}</td>
-                  <td >{item.desc}</td>
-                  <td style={item.saldo > 0 ? { color: "black" } : { color: "red" }} className='bg-transparent text-xl'>{item.saldo}</td>
-                  <td >{item.valorUni}</td>
-                  <td >{item.cat}</td>
+                  <td>{item.cod}</td>
+                  <td>{item.desc}</td>
+                  <td
+                    style={
+                      item.saldo > 0 ? { color: "black" } : { color: "red" }
+                    }
+                    className="bg-transparent text-xl"
+                  >
+                    {item.saldo}
+                  </td>
+                  <td>{item.valorUni}</td>
+                  <td>{item.cat}</td>
                   <td className="text-center justify-center items-center">
                     <BsFillPencilFill className="text-gray-900 cursor-pointer bg-transparent text-center w-[100%] mb-2" />
                   </td>
                 </tr>
-
               </tbody>
             ))}
-
           </table>
         </div>
-       <Modal />
+        <Modal />
       </main>
       <Footer />
     </div>
-  )
-}
+  );
+};
